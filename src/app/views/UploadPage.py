3148f5ae2bc4b82ec72db94842c3d733b8a72e49
Vivaan Wadhwa <vivaanw@gmail.com>
2024-09-23 16:20:34 -0700

    upload done

--- src/app/views/UploadPage.py
+++ src/app/views/UploadPage.py
@@ -57,6 +57,15 @@ class UploadPage(QMainWindow):
         self.zip_button.clicked.connect(self.select_zip_file)
         self.button_layout.addWidget(self.zip_button)
 
+        # Create a layout for the back button
+        self.back_button_layout = QHBoxLayout()
+        self.main_layout.addLayout(self.back_button_layout)
+
+        # Back button
+        self.back_button = QPushButton("Back")
+        self.back_button.clicked.connect(self.go_back)
+        self.back_button_layout.addWidget(self.back_button, alignment=Qt.AlignLeft)
+
         # Label to display the selected path
         self.path_label = QLabel("")
         self.path_label.setAlignment(Qt.AlignCenter)
@@ -70,18 +79,24 @@ class UploadPage(QMainWindow):
         folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
         if folder_path:
             self.path_label.setText(f"Selected Folder: {folder_path}")
+            self.navigate_to_next_page(folder_path)
 
     def select_zip_file(self):
         """Open file dialog to select a ZIP file"""
         zip_file, _ = QFileDialog.getOpenFileName(self, "Select ZIP File", "", "ZIP Files (*.zip)")
         if zip_file:
             self.path_label.setText(f"Selected ZIP File: {zip_file}")
-
-# if __name__ == "__main__":
-#     from PyQt5.QtWidgets import QApplication
-#     import sys
-
-#     app = QApplication(sys.argv)
-#     window = UploadPage()
-#     window.show()
-#     app.exec()
+            self.navigate_to_next_page(zip_file)
+    
+    def navigate_to_next_page(self, selected_path):
+        """Navigate to 1st step page"""
+        self.hide()  # Hide the current page
+        from app.views.overView import overviewPage
+        self.overviewPage = overviewPage(selected_path)
+        self.overviewPage.show()
+            
+    def go_back(self):
+        self.hide()  # Hide the current page
+        from app.views.MainWindow import MainWindow
+        self.main_window = MainWindow()  # Recreate the MainWindow
+        self.main_window.show()  # Show the MainWindow
