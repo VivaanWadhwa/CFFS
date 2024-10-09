6bbbd417e8bd9ce0a98e4c7ff5c1d9a4fb7c3e83
Vivaan Wadhwa <vivaanw@gmail.com>
2024-10-09 13:25:30 -0700

    Labeller high level design and code finished

--- src/core/labeller.py
+++ src/core/labeller.py
@@ -1,9 +1,11 @@
-"""Modules for reading xml files and creating dataframes from them"""
+""""""
 import os
 import xml.etree.ElementTree as et
-from typing import List
+from typing import List, Optional
 import pandas as pd
+import logging
 
+logging.basicConfig(level = logging.INFO, fprmat = "%(asctime)s -- %(levelname)s - %(message)s")
 
 class Labeller:
     """This class is designed for representing the preprocessed dataset"""
@@ -15,25 +17,48 @@ class Labeller:
     conversions = None
     restaurant_name = None
     def __init__(self, filepath: List[str], restaurant) -> None:
+        """Constructor for the Labeller class"""
+        self.filepaths = self._validate_filepath(filepath)
+        self.restaurant_name = restaurant
+        self.items = pd.DataFrame()
+        self.ingredients = pd.DataFrame()
+        self.preps = pd.DataFrame()
+        self.products = pd.DataFrame()
+        self.conversions = pd.DataFrame()
+        self.read_recipes()
+
+    def __repr__(self) -> str:
+        return (f"Labeller FOR {self.restaurant_name}\n"
+                f"Items: {self.items.shape[0]}\n"
+                f"Ingredients: {self.ingredients.shape[0]}\n"
+                f"Preps: {self.preps.shape[0]}\n"
+                f"Products: {self.products.shape[0]}\n"
+                f"Conversions: {self.conversions.shape[0]}")
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    @staticmethod
+    def _validate_filepath(filepath: List[str]) -> List[str]:
+        """Function for validating the filepath"""
+        if not isinstance(filepath, list):
+            raise ValueError("Filepath should be a list of strings.")
         for path in filepath:
             if not os.path.isdir(path):
-                raise FileNotFoundError(f"The path provided does not exist: {path}")
-        self.filepaths = filepath
-        self.restaurant_name = restaurant
-        self.read_recipes()
-    def __repr__(self) -> str:
-        string = f"Labeller FOR {self.restaurant_name}\nItems: {self.items.shape[0]}\nIngredients: {self.ingredients.shape[0]}\nPreps: {self.preps.shape[0]}\nProducts: {self.products.shape[0]}\nConversions: {self.conversions.shape[0]}"
-        return string
-    def __str__(self) -> str:
-        string = f"""
-        Labeller FOR {self.restaurant_name}
-        Items: {self.items.shape[0]}
-        Ingredients: {self.ingredients.shape[0]}
-        Preps: {self.preps.shape[0]}
-        Products: {self.products.shape[0]}
-        Conversions: {self.conversions.shape[0]}
-        """
-        return string
+                raise FileNotFoundError(f"Directory not found: {path}")
+        return filepath
+
+    # def _read_xml_file(self, filename: str, tag: str) -> Optional[et.ElementTree]:
+    #     """Function for parsing the XML File"""
+    #     for filepath in self.filepaths:
+    #         full_path = os.path.join(filepath, filename)
+    #         if os.path.isfile(full_path):
+    #             try:
+    #                 return et.parse(full_path)
+    #             except et.ParseError as e:
+    #                 logging.error(f"Error parsing {filename}: {e}")
+    #                 return None
+    #     return None
 
     def read_items(self) -> None:
         """Function for reading in all items required for recipes"""
@@ -50,7 +75,11 @@ class Labeller:
         for filepath in self.filepaths:
             path = filepath + '/Items.xml'
             if os.path.isfile(path):
-                xtree = et.parse(path)
+                try:
+                    xtree = et.parse(path)
+                except et.ParseError as e:
+                    logging.error("Error parsing Items.xml: %s", e)
+                    raise e
                 for item in xtree.iterfind('Item'):
                     itemid.append(item.attrib['id'])
                     description.append(item.findtext('Description'))
@@ -66,9 +95,7 @@ class Labeller:
                                    'CaseUOM': caseuom,
                                    'PakQty': pakqty,
                                    'PakUOM': pakuom,'InventoryGroup': inventorygroup})
-        self.items.drop_duplicates(inplace=True)
-        self.items.reset_index(drop=True, inplace=True)
-        self.items.dropna(inplace=True)
+        self._clean_dataframe(self.items, subset=["ItemId"])
     def read_ingredients(self) -> None:
         """Function for reading in all ingredients required for recipes"""
         # Read ingredients.xml files in the filepath_list and construct a dataframe
@@ -99,8 +126,7 @@ class Labeller:
                                          'Conversion': conversion,
                                          'InvFactor': invfactor,
                                          'Recipe': recipe}).drop_duplicates()
-        self.ingredients.drop_duplicates(subset=["IngredientId", "Recipe"], inplace=True)
-        self.ingredients.reset_index(drop=True, inplace=True)
+        self._clean_dataframe(self.ingredients, subset=["IngredientId", "Recipe"])
     def read_preps(self) -> None:
         """Function for reading in all preparations required for recipes"""
         prepid = []
@@ -123,8 +149,7 @@ class Labeller:
                                    'PakQty': pakqty,
                                    'PakUOM':pakuom,
                                    'InventoryGroup': inventorygroup}).drop_duplicates()
-        self.preps.drop_duplicates(subset=["PrepId"], inplace=True)
-        self.preps.reset_index(drop=True, inplace=True)
+        self._clean_dataframe(self.preps, subset=["PrepId"])
     def read_products(self) -> None:
         """Function for reading in all recipes Names"""
         prodid = []
@@ -141,8 +166,7 @@ class Labeller:
         self.products = pd.DataFrame({'ProdId': prodid,
                                       'Description': description,
                                       'SalesGroup': salesgroup})
-        self.products.drop_duplicates(inplace=True)
-        self.products.reset_index(drop=True, inplace=True)
+        self._clean_dataframe(self.products, subset=["ProdId"])
     def read_conversions(self) -> None:
         """Function for reading in all conversions required for recipes"""
         conversionid = []
@@ -168,7 +192,13 @@ class Labeller:
                                         'ConvertToQty': converttoqty,
                                         'ConvertToUom': converttouom}
                                 ).drop_duplicates()
-        self.conversions.reset_index(drop=True, inplace=True)
+        self._clean_dataframe(self.conversions)
+  
+    def _clean_dataframe(self, df: pd.DataFrame, subset: Optional[List[str]] = None) -> None:
+        """Function for cleaning the dataframe"""
+        df.drop_duplicates(subset=subset, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        df.dropna(inplace=True)
     def read_recipes(self):
         """Function for reading in all recipes"""
         self.read_items()
